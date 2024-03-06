f32eb60ac4047e7bae3f62e0f99cd9b6f31af3bf
kingmeky <emeka.andrew.ekeke@gmail.com>
2024-03-06 14:33:02 -0500

    Adding blood pressure and heart rate computed properties for heart condition screen

--- Capstone_Project/HeartConditionScreenView.swift
+++ Capstone_Project/HeartConditionScreenView.swift
@@ -22,22 +22,11 @@ struct HeartConditionScreenView: View {
             }
         }
     
-    var heartRateStatus: String {
-        let heartRate = sharedData.heartRateValue
-        if heartRate > 100 {
-            return "Your heart rate is high. Please consult your doctor."
-        } else if heartRate < 60 {
-            return "Your heart rate is low. Please consult your doctor."
-        } else {
-            return "Your heart rate is normal."
-        }
-    }
-
+    
     
     var body: some View {
         ScrollView {
             VStack {
-                // Title Text
                 Text("Heart Condition")
                     .font(.custom("Rockwell", size: 30))
                     .fontWeight(.semibold)
